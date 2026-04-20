89ea962c63f28794fc6845a0ccf25501679c71c7
Abhoy Kumar Mondal <workwithabhoy@gmail.com>
2026-04-20 22:14:36 +0530

    Fix duplicate subnet CIDR - public subnets now use 10.0.1.0/24 and 10.0.2.0/24

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -17,7 +17,7 @@ variable "environment" {
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets(one per AZ)"
   type = list(string)
-  default = ["10.0.1.0/24","10.0.20.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 
 }
 
